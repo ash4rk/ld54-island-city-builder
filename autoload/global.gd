36fb9ec926d0d0678ce53491ceaa053a76d83503
Alexey Akulov <alexey.akulov@atom.team>
2023-10-02 03:37:37 +0300

    FIX: Emitting coin particles

--- autoload/global.gd
+++ autoload/global.gd
@@ -10,6 +10,7 @@ var current_step: int = 0
 
 signal update_stats()
 signal next_move()
+signal collect_income()
 signal game_over()
 
 func game_over():
@@ -22,9 +23,13 @@ func update_stats():
 
 func next_move():
 	print_debug("next_move_event")
-	self.coins += income
-	current_step += 1
+
 	emit_signal("next_move")
+	
+func collect_income():
+	print_debug("collect_income_event")
+	self.coins += income
+	emit_signal("collect_income")
 
 func _set_coins(new_value):
 	coins = new_value
@@ -49,6 +54,8 @@ func game_loop():
 		update_stats()
 		
 		yield(self, "next_move")
+		current_step += 1
+		collect_income()
 	
 	game_over()
 
--- scenes/building.gd
+++ scenes/building.gd
@@ -4,12 +4,18 @@ extends Node
 class_name Building
 
 export var building_info: Resource
+onready var income_particle: CPUParticles = $IncomeParticle
 
 func _ready():
 	self.add_child(building_info.mesh.instance())
-	Global.connect("update_stats", self, "_emit_income")
+	Global.connect("collect_income", self, "_emit_income")
 	if building_info.particle_image:
-		$IncomeParticle.mesh.material.albedo_texture = building_info.particle_image
+		income_particle.mesh.material.albedo_texture = building_info.particle_image
+		income_particle.mesh.material.flags_transparent = true
+		income_particle.mesh.material.flags_unshaded = true
+		income_particle.mesh.size = Vector2(3.0, 3.0)
 	
 func _emit_income():
-	$IncomeParticle.emitting = true
+	if income_particle.emitting:
+		add_child(income_particle.duplicate())
+	income_particle.emitting = true
