cd1c3b48ad3b64f5bf59e8b05be9f045850980d3
ash4rk <cool.shark.98@mail.ru>
2023-10-02 06:10:55 +0300

    FEAT(WIP): Implement GameOverScreen

--- scripts/game_over_screen.gd
+++ scripts/game_over_screen.gd
@@ -1,5 +1,13 @@
 extends Control
 
+const REQUERED_BUDGET: int = 500
+const LOW_LEVEL_ATTRACTIVENESS: int = 0
+const HIGH_LEVEL_ATTRACTIVENESS: int = 25
+const LOW_LEVEL_TECHNOLOGIES: int = 0
+const HIGH_LEVEL_TECHNOLOGIES: int = 25
+
+onready var green_panel_stylebox = preload("res://assets/green_panel_style_box.tres")
+onready var red_panel_stylebox = preload("res://assets/red_panel_style_box.tres")
 onready var restart_button = $VBoxContainer/HBoxContainer/RestartButton
 onready var overview_button = $VBoxContainer/HBoxContainer/OverviewButton
 
@@ -7,6 +15,7 @@ func _ready():
 	Global.connect("game_over", self, "_game_over_event")
 	restart_button.connect("pressed", self, "_on_restart_button_pressed")
 	overview_button.connect("pressed", self, "_on_overview_button_pressed")
+	_counting_the_results()
 
 func _game_over_event():
 	_counting_the_results()
@@ -14,8 +23,7 @@ func _game_over_event():
 
 func _on_overview_button_pressed():
 	$BG.hide()
-	$VBoxContainer/BankruptcyMessage.hide()
-	$VBoxContainer/NormalMessage.hide()
+	$VBoxContainer/GameOverMessage.hide()
 	$VBoxContainer.anchor_bottom = 0.0
 	overview_button.hide()
 
@@ -24,6 +32,43 @@ func _on_restart_button_pressed():
 
 func _counting_the_results():
 	if Global.income <= 0 and Global.coins < 0:
-		$VBoxContainer/BankruptcyMessage.show()
+		# Bankruptcy Message
+		$VBoxContainer/GameOverMessage.add_stylebox_override("panel", red_panel_stylebox)
+		$VBoxContainer/GameOverMessage/VBoxContainer/CongratLabel.text = "Oh no, you're bankrupt!"
+		$VBoxContainer/GameOverMessage/VBoxContainer/MessageLabel.hide()
 	else:
-		$VBoxContainer/NormalMessage.show()
+		# Normal Message
+		$VBoxContainer/GameOverMessage.add_stylebox_override("panel", green_panel_stylebox)
+		$VBoxContainer/GameOverMessage/VBoxContainer/CongratLabel.text = "Congratulations!"
+		$VBoxContainer/GameOverMessage/VBoxContainer/MessageLabel.show()
+		$VBoxContainer/GameOverMessage/VBoxContainer/MessageLabel.text = "You've made some accomplishments!\n"
+		$VBoxContainer/GameOverMessage/VBoxContainer/MessageLabel.text += _generate_game_over_text()
+		
+	$VBoxContainer/GameOverMessage.show()
+	
+func _generate_game_over_text():
+	var game_over_text = "\n"
+	
+	# Budget check
+	if Global.coins > REQUERED_BUDGET:
+		game_over_text += "GOOD BUDGET!\n"
+	else:
+		game_over_text += "BAD BUDGET!\n"
+
+	# Technologies check
+	if Global.technologies < LOW_LEVEL_TECHNOLOGIES:
+		game_over_text += "BAD TECHNOLOGIES!\n"
+	elif Global.technologies > LOW_LEVEL_TECHNOLOGIES and Global.attractiveness < HIGH_LEVEL_TECHNOLOGIES:
+		game_over_text += "MEAN TECHNOLOGIES!\n"
+	else:
+		game_over_text += "GOOD TECHNOLOGIES!\n"
+
+	# Attractiveness check
+	if Global.attractiveness < LOW_LEVEL_ATTRACTIVENESS:
+		game_over_text += "BAD ATTRACTIVENESS!"
+	elif Global.attractiveness > LOW_LEVEL_ATTRACTIVENESS and Global.attractiveness < HIGH_LEVEL_ATTRACTIVENESS:
+		game_over_text += "MEAN ATTRACTIVENESS!"
+	else:
+		game_over_text += "GOOD ATTRACTIVENESS!"
+	
+	return game_over_text
