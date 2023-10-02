c7d750092f7f3f9f282d301a52ba4095cdbd42f0
ash4rk <cool.shark.98@mail.ru>
2023-10-02 08:33:37 +0300

    FIX: Update panel, update progress bars

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,7 +2,7 @@ extends CenterContainer
 
 
 func _ready():
-	$VBoxContainer/StartButton.connect("pressed", self, "_on_start_button_pressed")
+	$PanelContainer/VBoxContainer/StartButton.connect("pressed", self, "_on_start_button_pressed")
 	
 func _on_start_button_pressed():
 	Global.reset()
